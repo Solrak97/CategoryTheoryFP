c7fd17f0e2f4a1487f35d6fcdf2b142f0f366260
solrak <luiscarlosquesada@gmail.com>
2022-01-31 21:43:40 -0600

    Las unitests son para los mediocres que no pueden probar matematicamente su funcion

---
+++ Categoria de Kleisli/Kelisli.hs
@@ -0,0 +1,31 @@
+module Main where
+
+-- base safe reciprocal
+safe_reciprocal :: Double -> Maybe Double
+safe_reciprocal x
+    | x == 0    =   Nothing
+    | otherwise =   Just (x ** (-1)) 
+
+
+safe_root :: Double -> Maybe Double
+safe_root x 
+    | x < 0     =   Nothing
+    | otherwise   =   Just (sqrt(x))
+
+
+safe_root_reciprocal :: Double -> Maybe Double
+safe_root_reciprocal x = case ( safe_reciprocal x ) of
+    Nothing -> Nothing
+    Just y -> case ( safe_root y ) of 
+        Nothing -> Nothing
+        Just z -> Just z
+
+main = do
+    print (safe_reciprocal 0)
+    print (safe_reciprocal 1)
+
+    print (safe_root (-1))
+    print (safe_root 4)
+
+    print (safe_root_reciprocal 0)
+    print (safe_root_reciprocal 8)
